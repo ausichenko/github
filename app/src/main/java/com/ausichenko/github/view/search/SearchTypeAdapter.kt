0e8d044c97f34b3a9f95a68c45f6a85f1fc79dee
Andrey Usichenko <usichenko.andrey@gmail.com>
2018-12-20 21:42:12 +0700

    Fix lint issues. Update ProGuard rules.

--- app/src/main/java/com/ausichenko/github/view/search/SearchTypeAdapter.kt
+++ app/src/main/java/com/ausichenko/github/view/search/SearchTypeAdapter.kt
@@ -2,10 +2,11 @@ package com.ausichenko.github.view.search
 
 import android.content.Context
 import android.widget.ArrayAdapter
+import com.ausichenko.github.R
 
 class SearchTypeAdapter(context: Context, items: List<SearchTypeAdapter.SearchItem>) :
     ArrayAdapter<SearchTypeAdapter.SearchItem>(
-        context, android.R.layout.simple_spinner_dropdown_item, items
+        context, R.layout.item_dropdown_spinner, items
     ) {
 
     override fun getItemId(position: Int): Long {
